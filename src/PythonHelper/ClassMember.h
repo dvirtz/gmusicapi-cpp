c106e1bd48b8b238ab931fb666b1017ce5349e38
dvirtz <dvirtz@gmail.com>
2016-01-03 23:33:30 +0200

    add missing file

---
+++ src/PythonHelper/ClassMember.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/*!
+define PythonHelper::ClassBase member variable
+\param type     the member type
+\param name     the member name
+\param parent   type of class holding the member variable
+*/
+#define CLASS_MEMBER(type, name, parent) \
+    struct name##_s \
+    { \
+        name##_s(parent##& p) : m_p(p) {} \
+        parent##& m_p; \
+        operator type () const\
+        { \
+            return m_p.getMember<type>(#name); \
+        } \
+        name##_s operator=(const type##& val) \
+        { \
+            m_p.setMember(#name, val); \
+            return *this; \
+        } \
+    }; \
+    name##_s name{*this}
