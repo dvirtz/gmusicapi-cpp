e38c85054133ed9126015a4c8f38186f16630495
dvirtz <dvirtz@gmail.com>
2016-03-04 00:00:26 +0200

    added user-config for boost

--- scripts/install-boost.sh
+++ scripts/install-boost.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e	
 # check to see if boost folder is empty
-if [ ! -d "$HOME/boost_1_60_0/stage" ]; then
+if [ ! -d "$HOME/boost_1_60_0/lib" ]; then
 	wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2 -O /tmp/boost_1_60_0.tar.bz2;
 	tar --bzip2 -xf /tmp/boost_1_60_0.tar.bz2;
 	cd boost_1_60_0;
