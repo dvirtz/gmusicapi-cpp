265283bdcb77dd755f56e2e39522b780c72647da
dvirtz <dvirtz@gmail.com>
2016-11-16 22:35:36 +0200

    remove gmusicapi_path

--- src/gmusicapi/Module.cpp
+++ src/gmusicapi/Module.cpp
@@ -1,11 +1,10 @@
 #include "gmusicapi/Module.h"
-#include "gmusicapiPath.h"
 
 namespace GMusicApi
 {
 
 Module::Module()
-    : PythonHelper::ModuleBase("gmusicapi", gmusicapi_path)
+    : PythonHelper::ModuleBase("gmusicapi")
 {
 }
 
