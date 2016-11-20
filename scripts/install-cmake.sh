1c64cb6bc8ca021d24586535f7eb1f952c002604
dvirtz <dvirtz@gmail.com>
2016-11-20 23:16:15 +0200

    install cmake version

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+VERSION=$1
 
 if [ ! -d /home/travis/cmake ]; then
-	wget --no-check-certificate https://cmake.org/files/v3.2/cmake-3.2.0-Linux-x86_64.sh
-	chmod +x cmake-3.2.0-Linux-x86_64.sh
+	wget --no-check-certificate https://cmake.org/files/v$VERSION/cmake-$VERSION.0-Linux-x86_64.sh
+	chmod +x cmake-$VERSION.0-Linux-x86_64.sh
 	mkdir /home/travis/cmake
-	./cmake-3.2.0-Linux-x86_64.sh --skip-license --prefix=/home/travis/cmake
+	./cmake-$VERSION.0-Linux-x86_64.sh --skip-license --prefix=/home/travis/cmake
 fi
 export PATH=/home/travis/cmake/bin:${PATH}
