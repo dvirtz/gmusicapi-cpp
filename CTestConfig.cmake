00dc62e83c126e2ec4d992ce7faf3c8e86fc293c
dvirtz <dvirtz@gmail.com>
2016-12-18 22:00:19 +0200

    added CDash support

---
+++ CTestConfig.cmake
@@ -0,0 +1,9 @@
+# The gmusicapi-cpp CDash page appears at
+#   http://my.cdash.org/index.php?project=gmusicapi-cpp
+set(CTEST_PROJECT_NAME "gmusicapi-cpp")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=gmusicapi-cpp")
+set(CTEST_DROP_SITE_CDASH TRUE)
